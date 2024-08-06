c7ad4ef54df8e5366c22c7280fb5a71841e83abc
sireesha-gif <hemasiri567@gmail.com>
2024-08-06 12:05:17 +0530

    nice work

---
+++ sum.py
@@ -0,0 +1,19 @@
+class Addition
+{
+int a,b,tot;
+void add()
+{
+a=5;
+b=10;
+tot=a+b;
+System.out.println(tot);
+}
+}
+class Maddition
+{
+public statc void main(String args[])
+{
+Addition ob=new Addition();
+ob.add();
+}
+}
